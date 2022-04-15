76abfb03aad1fd248e62faf79dcf3bed7abd5c3c
Cinnlight <cinnlight@gmail.com>
2022-04-15 14:22:42 -0600

    Finish exercise 01 & 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+let heyString = '';
 
+for (i = 0; i < num; i++) {
+    heyString += string;
+  }
+
+  if (num < 0) {
+    heyString = "ERROR";
+    return heyString;
+  } else {
+    return heyString;
+  }
 };
+
+repeatString("hey", -1)
 
 // Do not edit below this line
 module.exports = repeatString;
