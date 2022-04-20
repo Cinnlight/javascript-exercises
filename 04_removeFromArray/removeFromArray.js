c24ff9ae7eb901a566abeb3310c35fe28a91dcbc
Cinnlight <cinnlight@gmail.com>
2022-04-20 14:01:47 -0600

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+// Function that takes an array as its first argument
+// Function then removes all following arguments from the array
 
+const removeFromArray = function(array) {
+    let filteredArray = array;
+
+for (let i = 1; i < arguments.length; i++) {
+    filteredArray = filteredArray.filter(newArray => newArray !== arguments[i]);
+}
+    return filteredArray;
 };
 
 // Do not edit below this line
