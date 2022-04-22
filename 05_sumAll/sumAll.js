9f65e243f7037adbae0212bba04aafe77d803da6
Cinnlight <cinnlight@gmail.com>
2022-04-22 14:03:27 -0600

    Solved exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(init, end) {
+    let swap;
 
+    if (Array.isArray(init) === true || Array.isArray(end) === true) {
+        return "ERROR";
+    }
+    if (init < 0 || end < 0 || Number.isInteger(init) === false || Number.isInteger(end) === false) {
+        return "ERROR";
+    }
+    if (init > end) {
+        swap = init;
+        init = end;
+        end = swap;
+    }
+    
+    let sum = init;
+
+    for (i = init + 1; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
